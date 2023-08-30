7fa37d678fa4a5725f5ff518bffef6a553b6552d
witojs <witoirawan.js@gmail.com>
2023-08-30 19:44:58 +0700

    make test pass

--- src/states/authUser/reducer.js
+++ src/states/authUser/reducer.js
@@ -3,7 +3,7 @@ import { ActionType } from './action';
 function authUserReducer(authUser = null, action = {}) {
   switch (action.type) {
     case ActionType.SET_AUTH_USER:
-      action.payload.authUser;
+      return action.payload.authUser;
     case ActionType.UNSET_AUTH_USER:
       return null;
     default:
